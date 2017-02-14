d2b544427290762e90ef9f881199974fece9b785
anusada <anusada@greeny.cs.tlu.ee>
2017-02-14 08:45:05 +0200

    finished

--- Proov1.java
+++ Proov1.java
@@ -1,8 +1,8 @@
 public class Proov1{
 	public static void main(String[] args){
-		TaisTrapets laud1=new TaisTrapets(2,1,3,8);
-		TaisTrapets laud2=new TaisTrapets(1.5,1,5.5,7.8);
-		TaisTrapets laud3=new TaisTrapets(2, 1.3, 5, 6);
+		TaisTrapets laud1=new TaisTrapets(2,1,3,8,45.5);
+		TaisTrapets laud2=new TaisTrapets(1.5,1,5.5,7.8,12);
+		TaisTrapets laud3=new TaisTrapets(2, 1.3, 5, 6,60.6);
 		System.out.println(" ");
 		System.out.println("taisnurksete trapetsite pindalad on:");
 		System.out.println(laud1.pindala()+" ruutsentimeetrit");
@@ -18,22 +18,12 @@ public class Proov1{
 		System.out.println(laud1.keskloik()+" sentimeetrit");
 		System.out.println(laud2.keskloik()+" sentimeetrit");
 		System.out.println(laud3.keskloik()+" sentimeetrit");
+		System.out.println(" ");
+		System.out.println("taisnurksete trapetsite nyrinurgad on:");
+		System.out.println(laud1.nyrinurk()+" kraadi");
+		System.out.println(laud2.nyrinurk()+" kraadi");
+		System.out.println(laud3.nyrinurk()+" kraadi");
+
 		
 	}
-/*VÄLJUND
-taisnurksete trapetsite pindalad on:
-8.0 ruutsentimeetrit
-6.15 ruutsentimeetrit
-7.9 ruutsentimeetrit
-
-taisnurksete trapetsite ymbermoodud on:
-12.0 sentimeetrit
-12.5 sentimeetrit
-14.3 sentimeetrit
-
-taisnurksete trapetsite keskloigud on:
-4.5 sentimeetrit
-2.75 sentimeetrit
-4.65 sentimeetrit
-*/
 }
--- TaisTrapets.java
+++ TaisTrapets.java
@@ -1,6 +1,6 @@
 public class TaisTrapets{
-	double a, x, b, h;
-	public TaisTrapets(double kylg, double alus, double hypotenuus, double korgus){
+	double a, x, b, h, t;
+	public TaisTrapets(double kylg, double alus, double hypotenuus, double korgus, double teravnurk){
 		if(kylg<=0){
 			throw new RuntimeException("Sobimatu");}
 		if(alus<=0){
@@ -9,38 +9,51 @@ public class TaisTrapets{
 			throw new RuntimeException("Sobimatu");}
 		if(korgus<=0){
 			throw new RuntimeException("Sobimatu");}
+		if(teravnurk>=90){
+			throw new RuntimeException("Sobimatu");}
+			
 		a=kylg;
 		x=alus;
 		b=hypotenuus;
 		h=korgus;
+		t=teravnurk;
 	}
 	public double pindala(){
-		return (a*a)+(x*h/2);
+		return (a*h)+(x*h/2);
 	}
 	
 	public double ymbermoot(){
-		return (4*a+x+b);
+		return (2*a+x+b+h);
 	}
 
 	/*Leian täisnurkse trapetsi kesklõigu pikkuse*/
 	public double keskloik(){
-		return ((a*a)+(x*h/2)/h);
+		return ((a+(a+x))/2);
+	}
+	/*Leian täisnurkse trapetsi nürinurga suuruse kraadides*/
+	public double nyrinurk(){
+		return (180-t);
 	}
 
 /*VÄLJUND
 taisnurksete trapetsite pindalad on:
-8.0 ruutsentimeetrit
-6.15 ruutsentimeetrit
-7.9 ruutsentimeetrit
+20.0 ruutsentimeetrit
+15.6 ruutsentimeetrit
+15.9 ruutsentimeetrit
 
 taisnurksete trapetsite ymbermoodud on:
-12.0 sentimeetrit
-12.5 sentimeetrit
-14.3 sentimeetrit
+16.0 sentimeetrit
+17.3 sentimeetrit
+16.3 sentimeetrit
 
 taisnurksete trapetsite keskloigud on:
-4.5 sentimeetrit
-2.75 sentimeetrit
-4.65 sentimeetrit
+2.5 sentimeetrit
+2.0 sentimeetrit
+2.65 sentimeetrit
+
+taisnurksete trapetsite nyrinurgad on:
+134.5 kraadi
+168.0 kraadi
+119.4 kraadi
 */
 }
